572b83adff12ec1aa6ab58c4fcef6d5087cd6439
GabrieleMeoni <gabriele.meoni@esa.int>
2023-08-30 14:47:34 +0200

    Applying docstring.

--- onboard_prototype/onboard_detector_processor.py
+++ onboard_prototype/onboard_detector_processor.py
@@ -29,6 +29,21 @@ class onboardDetectorProcessor:
         patch_overlap=0,
         device=torch.device("cpu"),
     ):
+        """Onboard Detector Processor init function.
+
+        Args:
+            satellite (str): satellite name (S2A or S2B).
+            detector_number (int): detector number.
+            model_name (str, optional): model name. Defaults to "efficientnet-lite0".
+            num_classes (int, optional): number of classes. Defaults to 2.
+            in_channels (int, optional): number of input channels. Defaults to 3.
+            depth (int, optional): depth factor for efficientnet-lite0. Defaults to 28.
+            widen_factor (int, optional): widen_factor for efficientnet-lite0. Defaults to 2.
+            leaky_slope (float, optional): leaky_slope for efficientnet-lite0. Defaults to 0.1.
+            dropout (float, optional): drop-out rate for efficientnet-lite0. Defaults to 0.0.
+            patch_overlap (int, optional): patch overlap during patch creation. Defaults to 0.
+            device (torch.device, optional): torch device. Defaults to torch.device("cpu").
+        """
         self.satellite = satellite
         self.detector_number = detector_number
 
@@ -59,15 +74,30 @@ class onboardDetectorProcessor:
         self.device = device
 
     def flush(self):
+        """Flush function to reset the prototype. It sets to None internal variables.
+        """
         self.x_coreg_pad = None
         self.x_prev = None
 
     def load_model(self, checkpoint_path):
+        """load pretrained model from checkpoint_path.
+
+        Args:
+            checkpoint_path (str): checkpoint path.
+        """
         checkpoint = torch.load(checkpoint_path)
         load_model = checkpoint["eval_model"]
         self.ai_model.load_state_dict(load_model)
 
     def coarse_coregistration(self, x):
+        """It applies the coarse coregistration to x.
+
+        Args:
+            x (torch.tensor): granule to register.
+
+        Returns:
+            torch.tensor: registered granule.
+        """
         x_coreg = coarse_coregistration(
             x,
             self.satellite,
@@ -79,6 +109,11 @@ class onboardDetectorProcessor:
         return x_coreg
 
     def init_patch_engine(self, x):
+        """Init patch engine.
+
+        Args:
+            x (torch.tensor): granule.
+        """
         n_v, n_h = int(x.shape[1] / 256), int(x.shape[2] / 256)
         self.x_coreg_pad = pad(
             x,
@@ -95,9 +130,19 @@ class onboardDetectorProcessor:
         self.finished = False
 
     def processing_finished(self):
+        """Check if the processing finished.
+
+        Returns:
+            bool: finished status.
+        """
         return self.finished
 
     def get_next_patch(self):
+        """Returns the next patch.
+
+        Returns:
+            torch.tensor: next patch.
+        """
         x_patch = self.x_coreg_pad[:, self.v : self.v + 256, self.h : self.h + 256]
 
         if self.v < self.x_coreg_pad.shape[1] - (256 - self.patch_overlap):
@@ -111,11 +156,27 @@ class onboardDetectorProcessor:
         return x_patch
 
     def get_output_shape(self):
+        """Returns the output shape of the output tensor.
+
+        Returns:
+            list: output shape [V_MAX, H_MAX]
+        """
         n_v_max = int(np.ceil(self.x_coreg_pad.shape[1] / (256 - self.patch_overlap)))
         n_h_max = int(np.ceil(self.x_coreg_pad.shape[2] / (256 - self.patch_overlap)))
         return [n_v_max, n_h_max]
 
     def process(self, x):
+        """Apply processing pipeline to a granule (coarse coregistration, demosaicking, AI inference, mosaicking)
+
+        Args:
+            x (torch.tensor): granule to process.
+
+        Raises:
+            ValueError: No model loaded
+
+        Returns:
+            torch.tensor: coarse segmentation. tensor containing the class predicted for each patch.
+        """
         # Applying coarse coregistration
         if self.ai_model is None:
             raise ValueError(
